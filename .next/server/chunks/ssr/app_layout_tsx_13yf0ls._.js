b54c7fa9f4dc718205ae65f573cc95c6063330ef
Fan Kun <fk@keyvalues.cn>
2026-04-16 15:37:33 +0000

    Remove node_modules

--- .next/server/chunks/ssr/app_layout_tsx_13yf0ls._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[33290,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"zh-CN",children:(0,b.jsx)("body",{className:"antialiased",children:a})})},"metadata",0,{title:"范坤 | 项目经理",description:"资深 IT 项目交付管理 · PMP 认证 · 系统集成与数字化转型"}])},70864,a=>{a.n(a.i(33290))}];
-
-//# sourceMappingURL=app_layout_tsx_13yf0ls._.js.map
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ezHizJevB8V/My16345UpaDrlTYsiFyjifw6jSOBDDI=\"\n}"
